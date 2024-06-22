e13623f5f288c7304514f568c8afe71d4db308c7
MahmoodAhmed-SE <Mahmood.Ahmed.SE@outlook.com>
2024-06-23 01:24:18 +0400

    added some sample logic for db connection with select query

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module movie-rating-api-go
 
 go 1.22.4
 
-require golang.org/x/crypto v0.24.0 // indirect
+require golang.org/x/crypto v0.24.0
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/jackc/pgx"
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type RequestBody struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
 }
 
 func handleUserRegistration(w http.ResponseWriter, r *http.Request) {
@@ -24,10 +35,10 @@ func handleUserRegistration(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	// saving user data into postgresdb
-	hashedPasswordBytes, err := bcrypt.GenerateFromPassword([]byte(data.Password), bcrypt.DefaultCost)
+	hashedPasswordBytes, hashErr := bcrypt.GenerateFromPassword([]byte(data.Password), bcrypt.DefaultCost)
 
-	if err != nil {
-		switch err.(type) {
+	if hashErr != nil {
+		switch hashErr.(type) {
 		case bcrypt.InvalidCostError:
 			http.Error(w, "Invalid cost parameter", http.StatusBadRequest)
 		case bcrypt.InvalidHashPrefixError:
@@ -37,15 +48,52 @@ func handleUserRegistration(w http.ResponseWriter, r *http.Request) {
 		default:
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 		}
-		log.Printf("Error generating hash: %v", err)
+		log.Printf("Error generating hash: %v", hashErr)
+		return
+	}
+	fmt.Println(string(hashedPasswordBytes))
+	dbConfigeration := pgx.ConnConfig{
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		Host:     "localhost",
+		Database: "recordings",
+		Port:     5432,
+	}
+	conn, connErr := pgx.Connect(dbConfigeration)
+
+	if connErr != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
+	var albums []Album
+
+	albumsQuery, albumErrQuery := conn.Query("select * from album;")
+
+	if albumErrQuery != nil {
+		fmt.Printf("Error selecting album rows: %s", albumErrQuery.Error())
+	}
+
+	defer albumsQuery.Close()
+
+	for albumsQuery.Next() {
+		var alb Album
+		if err := albumsQuery.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		albums = append(albums, alb)
+		fmt.Println(alb.Title)
+	}
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	http.HandleFunc("/api/", handleUserRegistration)
+	http.HandleFunc("/api/register-user", handleUserRegistration)
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
