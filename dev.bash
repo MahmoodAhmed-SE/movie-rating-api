20ef5c491e6f0cbb2efd82ef29e9c9b05955adc8
Mahmood Ahmed <Mahmood.Ahmed.SE.@outlook.com>
2024-06-30 01:04:23 +0400

    refactor: deleted unnecessary bash script.

--- dev.bash
+++
@@ -1,9 +0,0 @@
-if [ $1 = "build" ]; then
-    echo "Building main.go..."
-    go build -o bin/main main.go
-elif [ $1 = "run" ]; then
-    echo "Running main.go..."
-    go run main.go
-else 
-    echo "Parameter error!"
-fi
