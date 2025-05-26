866dec034a4303e51c1eafb442a2bddff8225d23
Mahmood Ahmed <Mahmood.Ahmed.SE.@outlook.com>
2025-05-26 20:17:43 +0400

    fix: removed columns of movies table that we don't have information about

--- internals/database/postgres_init.sql
+++ internals/database/postgres_init.sql
@@ -8,18 +8,13 @@ CREATE TABLE IF NOT EXISTS USERS (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 CREATE TABLE IF NOT EXISTS MOVIES (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
-    images TEXT[],
-    release_date DATE,
-    director VARCHAR(100),
-    rating_average NUMERIC(3, 2) DEFAULT 0.0,
-    duration INT -- duration in minutes
 );
 
 CREATE TABLE IF NOT EXISTS RATINGS (
